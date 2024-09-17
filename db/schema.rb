36b88e09f6691c9e052267dcea0353d3c525bdd9
beherit197777 <beherit197777@gmail.com>
2024-09-17 10:46:32 +0300

    fix migration v2

--- db/schema.rb
+++ db/schema.rb
@@ -40,9 +40,15 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_04_121305) do
   end
 
   create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "category_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "likes_count", default: 0
+    t.index ["category_id"], name: "index_posts_on_category_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -61,4 +67,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_04_121305) do
   add_foreign_key "post_comments", "users"
   add_foreign_key "post_likes", "posts"
   add_foreign_key "post_likes", "users"
+  add_foreign_key "posts", "categories"
+  add_foreign_key "posts", "users"
 end
