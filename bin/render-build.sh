e91690db936d69fbb9bab5de8a728020c1db807d
beherit197777 <beherit197777@gmail.com>
2024-09-17 22:47:02 +0300

    add render-build.sh v8

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,11 +2,14 @@
 # exit on error
 set -o errexit
 
+# Установка конфигурации Bundle
 bundle config set --local without development
-make install
-make yarn_install
-make drop_db
-make db_migrate
-make db_seed
-make compile_assets
-make clear_assets222
+bundle install
+if [ -f yarn.lock ]; then
+  yarn install
+fi
+bundle exec rake db:drop || true
+bundle exec rake db:migrateи
+bundle exec rake db:seed
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
