37d3d695be5e73abeef4a09e0061e738645583a9
beherit197777 <beherit197777@gmail.com>
2024-10-14 20:16:01 +0300

    add tests Comment and Likes v7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_17_100523) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_17_100523) do
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
