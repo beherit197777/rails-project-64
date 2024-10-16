f16ca12dfcfb17610198281b3ee4a367e0068b47
beherit197777 <beherit197777@gmail.com>
2024-10-16 22:00:27 +0300

    add compose

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,9 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
   config.hosts << "https://collectiveblog-qrq4ijzs.b4a.run/"
+  config.hosts << "0.0.0.0"
+  config.hosts << "localhost"
+
 
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
